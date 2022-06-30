692736c77320d19ecdcff90cc065a97a9a2a78b8
Dell <Dell@DESKTOP-BEP9P9A>
2022-06-30 18:01:55 +0530

    first commit

---
+++ datatypefloat/src/com/cg/datatypefloat/data.java
@@ -0,0 +1,13 @@
+package com.cg.datatypefloat;
+
+public class data {
+
+	public static void main(String[] args) {
+		float f=20.5f;
+		int a=(int)f;
+		System.out.println(a);
+		System.out.println(f);
+		
+	}
+
+}
