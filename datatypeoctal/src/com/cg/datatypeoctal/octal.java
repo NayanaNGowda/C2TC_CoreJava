a50f84babb9c96edd5493147f870bb001a9a1447
Dell <Dell@DESKTOP-BEP9P9A>
2022-06-30 18:02:51 +0530

    first commit

---
+++ datatypeoctal/src/com/cg/datatypeoctal/octal.java
@@ -0,0 +1,13 @@
+package com.cg.datatypeoctal;
+
+public class octal {
+
+	public static void main(String[] args) {
+		int i=010;
+		int j=07;
+		System.out.println(i);
+		System.out.println(j);
+
+	}
+
+}
