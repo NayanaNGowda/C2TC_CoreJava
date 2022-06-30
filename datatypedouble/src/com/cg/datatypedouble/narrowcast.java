82be0695c42d987b7b699f622a4e6d6c9ba455d0
Dell <Dell@DESKTOP-BEP9P9A>
2022-06-30 18:01:05 +0530

    first commit

---
+++ datatypedouble/src/com/cg/datatypedouble/narrowcast.java
@@ -0,0 +1,13 @@
+package com.cg.datatypedouble;
+
+public class narrowcast {
+
+	public static void main(String[] args) {
+		double myDouble=8.8;
+		int myInt=(int)myDouble;
+		System.out.println(myInt);
+		System.out.println(myDouble);
+
+	}
+
+}
