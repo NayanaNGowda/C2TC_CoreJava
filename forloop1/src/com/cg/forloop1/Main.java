1ef3b11dfe7e0e3b900b915d2f74e710dc73eb55
Dell <Dell@DESKTOP-BEP9P9A>
2022-06-30 21:59:47 +0530

    first commit

--- forloop1/src/com/cg/forloop1/Main.java
+++ forloop1/src/com/cg/forloop1/Main.java
@@ -3,8 +3,8 @@ package com.cg.forloop1;
 public class Main {
 
 	public static void main(String[] args) {
-		for(int i=1;i<=3; i++) { //i-->rows
-			for(int j=1;j<=3;j++) {//j-->columns
+		for(int i=1;i<=3; i++) { 
+			for(int j=1;j<=3;j++) {
 			System.out.println(i+" "+j);
 		}
 		}
--- forloop4/src/com/cg/forloop4/PatternProgram.java
+++ forloop4/src/com/cg/forloop4/PatternProgram.java
@@ -1,14 +1,14 @@
-package com.cg.forloop3;
+package com.cg.forloop4;
 
 public class PatternProgram {
 
 	public static void main(String[] args) {
 		for(int i=1;i<=5;i++) {//rows
-			for(int j=1;j<=i;j++) {//columns
+			for(int j=1;j<=5-i+1;j++) {//columns
 				System.out.print("* ");
 				
 			}
-			System.out.println();
+			System.out.println(" ");
 		}
 
 	}
