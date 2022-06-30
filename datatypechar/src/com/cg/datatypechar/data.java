b9fd807abc777bb86a734ef4d956193a948bb8d6
Dell <Dell@DESKTOP-BEP9P9A>
2022-06-30 17:59:44 +0530

    first commit

---
+++ datatypechar/src/com/cg/datatypechar/data.java
@@ -0,0 +1,13 @@
+package com.cg.datatypechar;
+
+public class data {
+
+	public static void main(String[] args) {
+		char ch='c';
+		int num=88;
+		ch=(char)num;
+		System.out.println(ch);
+
+	}
+
+}
