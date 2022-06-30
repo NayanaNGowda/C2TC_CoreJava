e2ccf727c8b3d3e71d6712165676a328e458a637
Dell <Dell@DESKTOP-BEP9P9A>
2022-06-30 16:37:08 +0530

    first commit

---
+++ preincrement4/src/com/cg/preincrement4/Test.java
@@ -0,0 +1,11 @@
+package com.cg.preincrement4;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Y"+"O");
+		System.out.println('L');
+		System.out.println('O');
+	}
+
+}
